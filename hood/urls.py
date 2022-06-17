3475ba9096a40394231439e3b3b80126a774dac0
oscar9181 <oscarokola17@gmail.com>
2022-06-17 14:47:38 +0300

    defining functions

--- hood/urls.py
+++ hood/urls.py
@@ -1,2 +1,7 @@
-from django.urls import  path
+from django.urls import  URLPattern, path
 from . import views
+
+urlpatterns = [
+           
+           
+]
--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+    
+    
+    return render(request,'neighbour/home.html')
+
+def registerpage (request):
+    
+    return render(request,'neighbour/register.html')
+
+
+def loginpage (request):
+    
+    return render(request,'neighbour/login.html')
