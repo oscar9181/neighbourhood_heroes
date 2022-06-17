d69e7c15028644c91158497c1750d897e25f4baf
oscar9181 <oscarokola17@gmail.com>
2022-06-17 17:37:25 +0300

    create superuser

--- hood/admin.py
+++ hood/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import NeighbourHood,Profile
+from . models import NeighbourHood,Profile
 
 # Register your models here.
 admin.site.register(NeighbourHood)
