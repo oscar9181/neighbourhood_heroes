9fe4e504e9916f2fa5e67a86f954c03088e096ef
oscar9181 <oscarokola17@gmail.com>
2022-06-17 15:00:44 +0300

    url patterns

--- hood/urls.py
+++ hood/urls.py
@@ -1,7 +1,12 @@
 from django.urls import  URLPattern, path
+
+from hood.views import loginpage
 from . import views
 
 urlpatterns = [
-           
+    path('',views.home,name='home'),
+    
+    path('login/',views.loginpage,name='login'),    
+    path('register/',views.registerpage,name='register'),    
            
 ]
