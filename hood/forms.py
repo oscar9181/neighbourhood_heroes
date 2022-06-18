d42d33284d66e7111d4478eede1661ce22a81f86
oscar9181 <oscarokola17@gmail.com>
2022-06-18 10:36:14 +0300

    authentictaion

--- hood/forms.py
+++ hood/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from.models import User
+from django import forms
+
 
 class CreateUserForm(UserCreationForm):
     
--- hood/views.py
+++ hood/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate,login,logout
+
+
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,11 +13,30 @@ def home (request):
     
     return render(request,'neighbour/home.html')
 
-def registerpage (request):
+def registerpage(request):
+    form=CreateUserForm()
     
-    return render(request,'neighbour/register.html')
+    if request.method == 'POST':
+         form=CreateUserForm(request.POST)
+         if form.is_valid():
+             form.save()    
+             
+             user = form.cleaned_data.get('username')
+             messages.success(request,'Account has been created for' + user)    
+             return redirect('login')    
+                   
+                   
+    context={'form':form}
+    return render(request,'neighbour/register.html',context)
 
 
 def loginpage (request):
-    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        
     return render(request,'neighbour/login.html')
