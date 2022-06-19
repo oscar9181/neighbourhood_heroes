3ea5648fe4d42c81e0bb35ebbb199b887cdd664a
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-06-19 17:53:33 +0300

    updated changes

--- hood/models.py
+++ hood/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
 # Create your models here.
 class NeighbourHood(models.Model):
