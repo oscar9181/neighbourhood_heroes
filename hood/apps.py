6da710735a6f6bc47bf55a6155372c30a41a2813
oscar9181 <oscarokola17@gmail.com>
2022-06-20 09:14:19 +0300

    signals.py

--- hood/apps.py
+++ hood/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class HoodConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hood'
+    def ready(self):
+        import  hood.signals
+
+
+
