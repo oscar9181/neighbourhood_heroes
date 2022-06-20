697a3113a3626222cc52b57788d40faed97e1ef9
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-06-20 12:33:29 +0300

    business urls paths addeed

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,6 @@
 from django.urls import  URLPattern, path
 
-from hood.views import loginpage
+from hood.views import loginpage, BusinessCreateView, BusinessDeleteView, BusinessDetailView, BusinessListView, BusinessUpdateView
 from . import views
 
 urlpatterns = [
@@ -12,5 +12,11 @@ urlpatterns = [
     path('profile/',views.userprofile,name='profile'),
     path('community/',views.community,name='community'),       
     path('update/',views.update,name='update'),
-     path('search/',views.search,name='search')  
+    path('search/',views.search,name='search'),
+    path('',BusinessListView.as_view(),name='home'),
+    path('business/<int:pk>/',BusinessDetailView.as_view(), name='business-detail'),
+    path('business/new/', BusinessCreateView.as_view(), name='business-create'),
+    path('business/<int:pk>/update/',BusinessUpdateView.as_view(), name='business-update'),
+    path('business/<int:pk>/delete/',BusinessDeleteView.as_view(),name='business-delete'), 
+
 ]
